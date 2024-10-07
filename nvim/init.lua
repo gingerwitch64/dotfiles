7e19eee31a5d62eb6899032580d0df0bf1a4ec85
Nola Gerold <redpendrew@gmail.com>
2024-10-06 22:09:16 -0700

    Add autocomplete

--- nvim/init.lua
+++ nvim/init.lua
@@ -29,8 +29,8 @@ require("lazy").setup({
       lazy = false,
     },
     {
-      'nvim-lualine/lualine.nvim',
-      dependencies = { 'nvim-tree/nvim-web-devicons' }
+      "nvim-lualine/lualine.nvim",
+      dependencies = { "nvim-tree/nvim-web-devicons" }
     },
     {
       "folke/tokyonight.nvim",
@@ -42,20 +42,112 @@ require("lazy").setup({
       "nvim-treesitter/nvim-treesitter",
       lazy = false,
     },
+    {
+      "L3MON4D3/LuaSnip",
+      version = "v2.*",
+      build = "make install_jsregexp",
+    },
+    {
+      "hrsh7th/nvim-cmp",
+      event = { "InsertEnter", "CmdlineEnter" },
+      dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+      },
+    },
+    {
+      "saadparwaiz1/cmp_luasnip",
+    },
   },
   install = { colorscheme = { "tokyonight" } }, -- colorscheme for the plugin interface
   checker = { enabled = false }, -- auto-check for updates?
 })
 
-require('lualine').setup({
-  options = { theme = 'tokyonight' },
+require("lualine").setup({
+  options = { theme = "tokyonight" },
 })
 
 vim.cmd[[colorscheme tokyonight-night]]
 
-require'lspconfig'.clangd.setup{}
-require'lspconfig'.ruff.setup{}
-require'lspconfig'.rust_analyzer.setup{}
+local lspconfig = require("lspconfig")
+local luasnip = require("luasnip")
+local cmp = require("cmp")
+
+cmp.setup({
+  snippet = { expand = function(args) luasnip.lsp_expand(args.body) end, },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  mapping = {
+    -- "Super Tab" mappings from
+    -- https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#luasnip
+    ['<CR>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        if luasnip.expandable() then
+          luasnip.expand()
+        else
+          cmp.confirm({
+            select = true,
+          })
+        end
+      else
+        fallback()
+      end
+    end),
+
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.locally_jumpable(1) then
+        luasnip.jump(1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.locally_jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    -- end "Super Tab" mappings
+  },
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+  },
+  {
+    { name = "buffer" },
+  }),
+})
+
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {{ name = "buffer" }}
+})
+
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = "path" }
+  }, {
+    { name = "cmdline" }
+  }),
+  matching = { disallow_symbol_nonprefix_matching = false }
+})
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+lspconfig.clangd.setup        { capabilities = capabilities }
+lspconfig.ruff.setup          { capabilities = capabilities }
+lspconfig.rust_analyzer.setup { capabilities = capabilities }
 
 require("nvim-treesitter.configs").setup {
   ensure_installed = {
