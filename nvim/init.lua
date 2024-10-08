5a9daafc50dc674d30a31eda14607d28d753a57d
Nola Gerold <redpendrew@gmail.com>
2024-10-07 23:17:24 -0700

    Remove all of the autocomplete junk due to config difficulties

--- nvim/init.lua
+++ nvim/init.lua
@@ -43,25 +43,6 @@ require("lazy").setup({
       lazy = false,
     },
     {
-      "L3MON4D3/LuaSnip",
-      version = "v2.*",
-      build = "make install_jsregexp",
-      dependencies = { "rafamadriz/friendly-snippets" },
-    },
-    {
-      "hrsh7th/nvim-cmp",
-      event = { "InsertEnter", "CmdlineEnter" },
-      dependencies = {
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-      },
-    },
-    {
-      "saadparwaiz1/cmp_luasnip",
-    },
-    {
       "m4xshen/autoclose.nvim",
     },
   },
@@ -75,88 +56,12 @@ require("lualine").setup({
 
 vim.cmd[[colorscheme tokyonight-night]]
 
-local autoclose = require("autoclose")
+require("autoclose").setup()
+
 local lspconfig = require("lspconfig")
-local luasnip = require("luasnip")
-local cmp = require("cmp")
-
-cmp.setup({
-  snippet = { expand = function(args) luasnip.lsp_expand(args.body) end, },
-  window = {
-    completion = cmp.config.window.bordered(),
-    documentation = cmp.config.window.bordered(),
-  },
-  mapping = {
-    -- "Super Tab" mappings from
-    -- https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#luasnip
-    ['<CR>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        if luasnip.expandable() then
-          luasnip.expand()
-        else
-          cmp.confirm({
-            select = true,
-          })
-        end
-      else
-        fallback()
-      end
-    end),
-
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.locally_jumpable(1) then
-        luasnip.jump(1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.locally_jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-    -- end "Super Tab" mappings
-  },
-  sources = cmp.config.sources({
-    { name = "nvim_lsp" },
-    { name = "luasnip" },
-  },
-  {
-    { name = "buffer" },
-  }),
-})
-
-cmp.setup.cmdline({ '/', '?' }, {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = {{ name = "buffer" }}
-})
-
-cmp.setup.cmdline(':', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = "path" }
-  }, {
-    { name = "cmdline" }
-  }),
-  matching = { disallow_symbol_nonprefix_matching = false }
-})
-
-autoclose.setup()
-
-require("luasnip.loaders.from_vscode").lazy_load()
-
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
-lspconfig.clangd.setup        { capabilities = capabilities }
-lspconfig.ruff.setup          { capabilities = capabilities }
-lspconfig.rust_analyzer.setup { capabilities = capabilities }
+lspconfig.clangd.setup{}
+lspconfig.ruff.setup{}
+lspconfig.rust_analyzer.setup{}
 
 require("nvim-treesitter.configs").setup {
   ensure_installed = {
